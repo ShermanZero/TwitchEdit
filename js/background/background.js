852da026b9591e7df56e3678c7e6a0679719ccf2
ShermanZero <15kpsherman@gmail.com>
2019-08-21 00:01:06 -0400

    clip functionality

--- js/background/background.js
+++ js/background/background.js
@@ -16,3 +16,21 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     });
   }
 });
+
+
+chrome.runtime.onMessage.addListener (
+  function(request, sender, sendResponse) {
+    //if we receive the command to closeTab
+    if(request.cmd == "closeTab") {
+      //queries the active tabs
+      chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+          //close the only tab returned in the array
+          chrome.tabs.remove(tabs[0].id);
+
+          //log that we receieved the message
+          console.log("{TwitchEdit-BG} recieved message to close tab");
+      });
+    }
+
+    return true;
+  });
--- js/content/clip/inject_clipHTML.js
+++ js/content/clip/inject_clipHTML.js
@@ -6,7 +6,7 @@ var iconSize = 16;
 var copyright = "<!-- TwitchEdit COPYRIGHT (C) 2019 KIERAN SHERMAN | twitch.tv/shermanzero -->";
 var sizeReplace = "{size}";
 
-var insertionPoint, clipButtonHTML = {contents: ""};
+var clip, insertionPoint, clipButtonHTML = {contents: ""};
 
 //mutation observer watching for added nodes
 var observer = new MutationObserver(function(mutations) {
@@ -28,6 +28,17 @@ var observer = new MutationObserver(function(mutations) {
         if(node.hasAttribute('class') && node.getAttribute('class').includes('twitchedit')) {
           continue;
         } else
+        //if the node added is the original clip button
+        if(node.getAttribute('class') == 'clips-disabled__hover') {
+          clip = node.children[0];
+
+          //log that we found the original clip button
+          console.log('{TwitchEdit-Clip} found original clip button', clip);
+
+          //now inject the HTML
+          modifyViewer();
+          return;
+        } else
         //if all other tests pass, but if the node is not exactly what we are looking for, continue
         if(!(node.getAttribute('id') == 'default-player')) {
           continue;
@@ -40,14 +51,11 @@ var observer = new MutationObserver(function(mutations) {
         if(node.hasChildNodes()) {
           console.log('{TwitchEdit-Clip} ^found match^');
 
-          //marks the insertion point
+          //find the root insertion point
           insertionPoint = node.getElementsByClassName('player-buttons-right')[0].children[0].children[0];
+
+          //logs we found the insertion point
           console.log("{TwitchEdit-Clip} marked insertion point:", insertionPoint);
-
-          //modify the viewer and finish (no need to keep iterating through other additions)
-          modifyViewer();
-          break;
-
         } else {
           //if the node did not match our parameters
           console.log('{TwitchEdit-Clip} ^did not match^')
@@ -73,10 +81,29 @@ function modifyViewer() {
   //display the injection in the console
   console.log('{TwitchEdit-Clip} injected: ', editRoot.getElementsByClassName('twitchedit-clip')[0]);
 
+  var clipButton = document.getElementById('twitchedit-clip-button');
+
   //add an svg to the button (svg imported)
-  var img = document.getElementById('twitchedit-clip-button').appendChild(document.createElement('img'));
+  var img = clipButton.appendChild(document.createElement('img'));
   img.src = chrome.runtime.getURL('/icons/clip/svg/clip_128.svg');
+
+  //log that we modified the new button to include an image
   console.log('{TwitchEdit-Clip} modified button to include image', img);
+
+  //modify the new clip button to perform a function on click
+  clipButton.onclick = function() {
+    //click the original clip button
+    clip.click();
+
+    //send message to background script telling it to close the tab
+    chrome.runtime.sendMessage({cmd: "closeTab"}, function(response) {
+      //log that the background script closed the tab
+      console.log("{TwitchEdit-Clip} background script closed tab");
+    });
+  }
+
+  //log that we modififed the button to perform a function on click
+  console.log('{TwitchEdit-Clip} modified button to perform function on click');
 
   //successfully injected!
   console.log('{TwitchEdit-Clip} !!- HTML injection COMPLETED | You can now click on the edit icon to go to the clip editor -!!');
